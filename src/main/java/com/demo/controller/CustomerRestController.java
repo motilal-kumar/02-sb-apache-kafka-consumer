6ab571591093c3a6e252bb1f735c6187faf66e1c
motilal-kumar <motilal.kumar@neosoft.com>
2023-05-29 15:42:51 +0530

    Initial commit SpringBoot and apache kafka integration for string as well as json.

---
+++ src/main/java/com/demo/controller/CustomerRestController.java
@@ -0,0 +1,14 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * This class is used to handle user requests
+ * 
+ * @author Motilal Kumar.
+ *
+ */
+@RestController
+public class CustomerRestController {
+
+}
